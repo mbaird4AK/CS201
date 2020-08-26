b4f398d2d30a6ece5e109b7bd06be24361f1a943
mbaird4AK <mbaird4@alaska.edu>
2020-08-26 11:35:05 -0800

    Added game description

---
+++ HW0/HW0/Gamedescription.cpp
@@ -0,0 +1,15 @@
+/* Gamedescription.cpp
+Should output a discription of Divinity Original Sin 2
+Written by Martin Baird
+26th of August 2020
+
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "The game Divinity Original Sin 2 is a top down dungeon crawler adventure RPG\nIt is very much like a DND campaign with a cast of original characters\nLike the Lizard-man, The Red-Prince";
+
+    return 0;
+}
