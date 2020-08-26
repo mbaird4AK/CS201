8aa383b769c6012f9a065e46e7445f361ae059f5
mbaird4AK <mbaird4@alaska.edu>
2020-08-26 11:26:31 -0800

    Added hello and poem

---
+++ HW0/HW0/Hello.cpp
@@ -0,0 +1,16 @@
+/* Hello.cpp
+Should output Hello World
+Written by Martin Baird
+26th of August 2020
+
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\nHello World!\nHello World!\nHello World!\nHello World!\n";
+    
+    return 0;
+}
+
---
+++ HW0/HW0/Poem.cpp
@@ -0,0 +1,15 @@
+/* Poem.cpp
+Should output V speech
+Written by Martin Baird
+26th of August 2020
+
+*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Voila!\nIn view a humble vaudevillian veteran\n cast vicariously as both victim and villain\n by the vicissitudes of fate\n";
+
+    return 0;
+}
